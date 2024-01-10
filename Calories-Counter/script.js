e38bbf7e7c9cb59c3e5866022f63803c1082c68b
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-10 18:05:43 +0100

    Declare variables to interact with the DOM

---
+++ Calories-Counter/script.js
@@ -0,0 +1,7 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
