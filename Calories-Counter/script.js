3a7fe25cbb769cf9f326e9496142644217880b7d
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-11 20:15:51 +0100

    Add AddEntry function

--- Calories-Counter/script.js
+++ Calories-Counter/script.js
@@ -15,3 +15,20 @@ function cleanInputString(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
   }
+
+  function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input
+      type="number"
+      min="0"
+      id="${entryDropdown.value}-${entryNumber}-calories"
+      placeholder="Calories"
+    />`;
+    targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
+  }
+  
