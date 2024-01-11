e80074653ddf1df58fe2dec415538473b196c559
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-11 20:14:46 +0100

    Add cleanInputString and isInvalidInput functions

--- Calories-Counter/script.js
+++ Calories-Counter/script.js
@@ -5,3 +5,13 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+  }
+  
+  function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+  }
