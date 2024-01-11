78f060968d9b5f1b8a5bf49dcf66da9b531ebbb8
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-11 20:17:38 +0100

    Create calculateCalories function

--- Calories-Counter/script.js
+++ Calories-Counter/script.js
@@ -32,3 +32,38 @@ function cleanInputString(str) {
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
   }
   
+  function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+  
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+  
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  
+    if (isError) {
+      return;
+    }
+  
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+  
+    output.classList.remove('hide');
+  }
+  
