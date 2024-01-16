451df5f4b7ac02815b33016fbd1e63d84618715a
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-16 22:56:55 +0100

    Create sellWeapon and fightSlime functions

--- creating-role-playing-game/role-play.js
+++ creating-role-playing-game/role-play.js
@@ -148,3 +148,19 @@ function goTown() {
     }
   }
   
+  function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+  }
+  
+  function fightSlime() {
+    fighting = 0;
+    goFight();
+  }
