892b59accbffff06476dfd053b0ff65346e472c2
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-16 22:54:53 +0100

    Create buyHealth and buyWeapon functions

--- creating-role-playing-game/role-play.js
+++ creating-role-playing-game/role-play.js
@@ -117,3 +117,34 @@ function goTown() {
     update(locations[2]);
   }
   
+  function buyHealth() {
+    if (gold >= 10) {
+      gold -= 10;
+      health += 10;
+      goldText.innerText = gold;
+      healthText.innerText = health;
+    } else {
+      text.innerText = "You do not have enough gold to buy health.";
+    }
+  }
+  
+  function buyWeapon() {
+    if (currentWeapon < weapons.length - 1) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+      }
+    } else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
+    }
+  }
+  
