86ffea5512f5886f6d7e0cc9e0acce5a4c7edd9c
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-16 22:47:10 +0100

    Create monsters object array

--- creating-role-playing-game/role-play.js
+++ creating-role-playing-game/role-play.js
@@ -23,3 +23,21 @@ const weapons = [
     { name: 'claw hammer', power: 50 },
     { name: 'sword', power: 100 }
   ];
+
+  const monsters = [
+    {
+      name: "slime",
+      level: 2,
+      health: 15
+    },
+    {
+      name: "fanged beast",
+      level: 8,
+      health: 60
+    },
+    {
+      name: "dragon",
+      level: 20,
+      health: 300
+    }
+  ]
