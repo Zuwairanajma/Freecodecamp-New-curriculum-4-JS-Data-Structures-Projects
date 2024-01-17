1bf911c34222cd9966ac3e96eadab9fec2518932
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-17 21:49:52 +0100

    Create the pick guess function

--- creating-role-playing-game/role-play.js
+++ creating-role-playing-game/role-play.js
@@ -257,3 +257,27 @@ function goTown() {
   function pickEight() {
     pick(8);
   }
+
+  function pick(guess) {
+    let numbers = [];
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+      text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.indexOf(guess) !== -1) {
+      text.innerText += "Right! You win 20 gold!";
+      gold += 20;
+      goldText.innerText = gold;
+    } else {
+      text.innerText += "Wrong! You lose 10 health!";
+      health -= 10;
+      healthText.innerText = health;
+      if (health <= 0) {
+        lose();
+      }
+    }
+  }
+  
