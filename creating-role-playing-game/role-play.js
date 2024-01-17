c84946ca866819d159257d5edc8cb3fe1fd2483f
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-17 21:48:51 +0100

    Add restart, easterEgg, pickTwo and pickEight functions

--- creating-role-playing-game/role-play.js
+++ creating-role-playing-game/role-play.js
@@ -233,3 +233,27 @@ function goTown() {
   function winGame() {
     update(locations[6]);
   }
+
+  function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown();
+  }
+  
+  function easterEgg() {
+    update(locations[7]);
+  }
+  
+  function pickTwo() {
+    pick(2);
+  }
+  
+  function pickEight() {
+    pick(8);
+  }
