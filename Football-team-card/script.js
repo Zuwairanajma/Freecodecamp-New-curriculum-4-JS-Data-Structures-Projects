559eeef111460c3e7c410ba77c4f26f74ba88c7e
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-18 21:04:01 +0100

    Create variables to access the dom

---
+++ Football-team-card/script.js
@@ -0,0 +1,6 @@
+const teamName = document.getElementById("team");
+const typeOfSport = document.getElementById("sport");
+const worldCupYear = document.getElementById("year");
+const headCoach = document.getElementById("head-coach");
+const playerCards = document.getElementById("player-cards");
+const playersDropdownList = document.getElementById("players");
