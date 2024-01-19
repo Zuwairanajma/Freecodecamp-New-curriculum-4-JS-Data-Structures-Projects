c353d8cc058c785e4f59c489668f6435d5f0dc8a
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-19 22:24:54 +0100

    Create myFavouriteFootballTeam onject

--- Football-team-card/script.js
+++ Football-team-card/script.js
@@ -4,3 +4,13 @@ const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
+const myFavoriteFootballTeam = {
+    team: "Argentina",
+    sport: "Football",
+    year: 1986,
+    isWorldCupWinner: true,
+    headCoach: {
+      coachName: "Carlos Bilardo",
+      matches: 7,
+    },
+}
