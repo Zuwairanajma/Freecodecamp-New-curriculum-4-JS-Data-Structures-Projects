6d5a371232a073dd88b5105b8e49dba19134d357
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-21 00:25:21 +0100

    Create setPlayerCards function

--- Football-team-card/script.js
+++ Football-team-card/script.js
@@ -179,3 +179,19 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr
+    .map(
+      ({ name, position, number, isCaptain, nickname }) =>
+        `
+        <div class="player-card">
+          <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      `
+    )
+    .join("");
+};
