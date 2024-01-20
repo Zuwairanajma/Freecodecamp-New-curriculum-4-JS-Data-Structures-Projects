a8e185ad176ad9ca409ef67a7581ca2af7827723
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-21 00:29:53 +0100

    Add change event listener to playersDropDownList  and switch statements to setPlayerCards

--- Football-team-card/script.js
+++ Football-team-card/script.js
@@ -195,3 +195,36 @@ const setPlayerCards = (arr = players) => {
     )
     .join("");
 };
+
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(
+        players.filter((player) => player.position === "midfielder")
+      );
+      break;
+    case "defender":
+      setPlayerCards(
+        players.filter((player) => player.position === "defender")
+      );
+      break;
+      case "goalkeeper":
+        setPlayerCards(
+          players.filter((player) => player.position === "goalkeeper")
+        );
+        break;
+  
+
+        default:
+            setPlayerCards()
+            
+
+  }
+});
