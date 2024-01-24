61732dae82dac89308922801540c0362971baae4
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-24 03:14:47 +0100

    Create event listeners for date options select element

--- Date-Formatter/script.js
+++ Date-Formatter/script.js
@@ -10,3 +10,21 @@ const minutes = date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+
+    switch (dateOptionsSelectElement.value) {
+      case "yyyy-mm-dd":
+        currentDateParagraph.textContent = formattedDate
+          .split("-")
+          .reverse()
+          .join("-");
+        break;
+      case "mm-dd-yyyy-h-mm":
+        currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+        break;
+      default: 
+      currentDateParagraph.textContent = formattedDate;
+    }
+  });
+  
