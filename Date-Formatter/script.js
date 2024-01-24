9ca1c5fa97c22099d7439dbeb2cf2f95e5fed354
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-01-24 03:12:59 +0100

    Add formatted date function

--- Date-Formatter/script.js
+++ Date-Formatter/script.js
@@ -7,3 +7,6 @@ const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
