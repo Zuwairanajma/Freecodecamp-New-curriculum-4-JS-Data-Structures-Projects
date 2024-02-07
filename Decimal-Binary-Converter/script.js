adef264b840040fa14e76dbf8c46b5d54ca2878b
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-02-07 03:58:45 +0100

    Create variables from the DOM

---
+++ Decimal-Binary-Converter/script.js
@@ -0,0 +1,4 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
