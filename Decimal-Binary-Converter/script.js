13916e5c8eb1450d04780c1aa2ec3013796c330b
Zuwairanajma <juwairiyyasadiq@gmail.com>
2024-03-04 18:46:09 +0100

    decimaltoBinary function

--- Decimal-Binary-Converter/script.js
+++ Decimal-Binary-Converter/script.js
@@ -30,3 +30,10 @@ const animationData = [
     }
   ];
   
+  const decimalToBinary = (input) => {
+    if (input === 0 || input === 1) {
+      return String(input);
+    } else {
+      return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+    }
+  };
